847f34c5f02770e572b63df7a60bf8c9cdf5de16
Mahad Abbas <mahadabbas31@gmail.com>
2026-05-03 00:56:50 +0500

    Getting Started with JAVA

---
+++ firstJavaCode.java
@@ -0,0 +1,14 @@
+/*
+-> Pro Tips (IMPORTANT)
+    ✔ Always name file = class name
+    ✔ One public class per file
+    ✔ Use camelCase (Java style)
+    ✔ Compile errors carefully (Java is strict)
+*/
+public class firstJavaCode{
+    public static void main(String[] args){
+        System.out.print("I am Mahad Abbas\n");
+        System.out.println("Today is April 23, 2026");
+        System.out.println("Getting Started with JAVA Programming");
+    }
+}
