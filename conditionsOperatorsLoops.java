69c1d812664ae5a212be7a027b1aabe1d49eeab4
Mahad Abbas <mahadabbas31@gmail.com>
2026-05-06 23:16:33 +0500

    main file

---
+++ conditionsOperatorsLoops.java
@@ -0,0 +1,17 @@
+public class conditionsOperatorsLoops {
+    public static void main(String arg[]){
+        minCalculator m = new minCalculator();
+
+        float a = m.add(14,14);
+        System.out.println("Result: " + a);
+        
+        a = m.multiply(14,14);
+        System.out.println("Result: " + a);
+        
+        a = m.subtract(14,14);
+        System.out.println("Result: " + a);
+        
+        a = m.divide(14,14);
+        System.out.println("Result: " + a);
+    }
+}
