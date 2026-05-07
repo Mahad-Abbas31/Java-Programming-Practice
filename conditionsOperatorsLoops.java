642f5e998d3947a3c41e4dd65fd64f8d40ef9d82
Mahad Abbas <mahadabbas31@gmail.com>
2026-05-07 20:44:32 +0500

    Using MiniCalculator Class Display Functions

--- conditionsOperatorsLoops.java
+++ conditionsOperatorsLoops.java
@@ -2,16 +2,6 @@ public class conditionsOperatorsLoops {
     public static void main(String arg[]){
         minCalculator m = new minCalculator();
 
-        float a = m.add(14,14);
-        System.out.println("Result: " + a);
-        
-        a = m.multiply(14,14);
-        System.out.println("Result: " + a);
-        
-        a = m.subtract(14,14);
-        System.out.println("Result: " + a);
-        
-        a = m.divide(14,14);
-        System.out.println("Result: " + a);
+        m.displayFunctions();
     }
 }
